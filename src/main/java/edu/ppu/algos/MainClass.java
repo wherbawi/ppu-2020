1fc36d4f670b28ed449a1247ab7022879a93e58c
Wesam Herbawi <wesam.herbawi@gmail.com>
2020-09-01 20:07:50 +0200

    changed dep test

--- src/main/java/edu/ppu/algos/MainClass.java
+++ src/main/java/edu/ppu/algos/MainClass.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
 
 public class MainClass {
     public static void main(String[] args) throws IOException, InterruptedException {
-        FileWriter fileWriter = new FileWriter(new File("ppu.txt"));
+        FileWriter fileWriter = new FileWriter(new File("ppu-dep-test.txt"));
             fileWriter.write(Calendar.getInstance().getTime().toString() + "\n");
             fileWriter.close();
 
