5f6d8460946d8bb673ae6d5d57fd26da5b4c5db2
Wesam Herbawi <wesam.herbawi@gmail.com>
2020-09-01 20:34:25 +0200

    t

--- src/main/java/edu/ppu/algos/MainClass.java
+++ src/main/java/edu/ppu/algos/MainClass.java
@@ -9,7 +9,6 @@ public class MainClass {
     public static void main(String[] args) throws IOException, InterruptedException {
         FileWriter fileWriter = new FileWriter(new File("ppu-dep-test.txt"));
             fileWriter.write(Calendar.getInstance().getTime().toString() + "\n");
-            fileWriter.flush();
             fileWriter.close();
 
     }
