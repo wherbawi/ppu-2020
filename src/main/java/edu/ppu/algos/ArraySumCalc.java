35c0b2814034ee9f0d7e36d8af0ffa9fb42bb540
Wesam Herbawi <wesam.herbawi@gmail.com>
2020-10-23 20:18:12 +0200

    reverted to springboot

--- src/main/java/edu/ppu/algos/ArraySumCalc.java
+++ src/main/java/edu/ppu/algos/ArraySumCalc.java
@@ -1,10 +1,10 @@
 package edu.ppu.algos;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-//@Component
+@Component
 public class ArraySumCalc {
     public int sum(int[] nums){
         return Arrays.stream(nums).sum();
